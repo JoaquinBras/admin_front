6cdfb2196a500b06647f80f03e1bebb598918664
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-06-21 13:09:40 -0300

    Admin Register

--- src/pages/AdminRegister.jsx
+++ src/pages/AdminRegister.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import '../css/Forms.css'
 
-const RegisterAdminForm = () => {
+const AdminRegisterForm = () => {
+  const [firstname, setFirstName] = useState('')
+  const [lastname, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleFormSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      const formData = new FormData()
+      formData.append('firstname', firstname)
+      formData.append('lastname', lastname)
+      formData.append('email', email)
+      formData.append('password', password)
+
+      const response = await axios.post(
+        'http://localhost:3000/admin/register',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+      )
+
+      console.log(response.data)
+      setFirstName('')
+      setLastName('')
+      setEmail('')
+      setPassword('')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="container d-grid align-items-center justify-content-center">
       <div
@@ -9,11 +45,11 @@ const RegisterAdminForm = () => {
         id="contenedor"
       >
         <div>
-          <h3 className="text-muted">Registrar administrador</h3>
+          <h3 className="text-muted">Registro de administrador</h3>
         </div>
         <hr />
-        <form method="post" action="">
-          <label htmlFor="firstname" className="mb-2 text-muted fw-bold">
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="name" className="mb-2 text-muted fw-bold">
             Nombre
           </label>
           <input
@@ -22,9 +58,12 @@ const RegisterAdminForm = () => {
             id="firstname"
             name="firstname"
             className="form-control mb-3"
-            placeholder="Ingrese su nombre.."
+            placeholder="Ingrese nombre de administrador.."
+            value={firstname}
+            onChange={e => setFirstName(e.target.value)}
+            required
           />
-          <label htmlFor="lastname" className="mb-2 text-muted fw-bold">
+          <label htmlFor="name" className="mb-2 text-muted fw-bold">
             Apellido
           </label>
           <input
@@ -32,17 +71,23 @@ const RegisterAdminForm = () => {
             id="lastname"
             name="lastname"
             className="form-control mb-3"
-            placeholder="Ingrese su apellido.."
+            placeholder="Ingrese apellido de administrador.."
+            value={lastname}
+            onChange={e => setLastName(e.target.value)}
+            required
           />
           <label htmlFor="email" className="mb-2 text-muted fw-bold">
-            Correo
+            Email
           </label>
           <input
             type="email"
             id="email"
             name="email"
             className="form-control mb-3"
-            placeholder="Ingrese su correo.."
+            placeholder="Ingrese email.."
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
           />
           <label htmlFor="password" className="mb-2 text-muted fw-bold">
             Contraseña
@@ -52,10 +97,12 @@ const RegisterAdminForm = () => {
             id="password"
             name="password"
             className="form-control mb-3"
-            placeholder="Ingrese su contraseña.."
+            placeholder="Ingrese contraseña.."
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
           />
-
-          <button className="btn btn-success mb-2 mt-2" type="submit">
+          <button className="btn btn-success mb-2 mt-3" type="submit">
             Registrar
           </button>
         </form>
@@ -64,4 +111,4 @@ const RegisterAdminForm = () => {
   )
 }
 
-export default RegisterAdminForm
+export default AdminRegisterForm
