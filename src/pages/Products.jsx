79bdc707a2906fc8fab2e9817887c6d2415b8dfa
JoaquinBras <joaquin.brascesco@gmail.com>
2023-06-22 15:54:21 -0300

    Vista Products del admin

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import "../css/Home.css";
-import "../css/Dashboard.css";
-import SideBar from "../components/Sidebar";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../components/Product";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    listProducts();
+  }, []);
+
+  async function listProducts() {
+    try {
+      const response = await axios.get("http://localhost:3000/product/");
+      const productList = Array.isArray(response.data.products)
+        ? response.data.products
+        : [];
+      setProducts(productList);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <section className="row p-0">
-      <SideBar />
-
-      <div id="contentArea" className="col-md-10 col-sm-9">
+      <div className="col-md-10 col-sm-9">
         <div className="d-flex justify-content-between align-items-center mt-2 mb-3">
           <h2>PRODUCTOS</h2>
           <button className="btn btn-success me-3" href="/articulos/crear">
@@ -24,12 +37,23 @@ function Products() {
               <th scope="col">Id</th>
               <th scope="col">Producto</th>
               <th scope="col">Descripción</th>
-              <th scope="col">Image</th>
+              <th scope="col">Imagen</th>
               <th className="text-center" scope="col">
-                Actions
+                Acciones
               </th>
             </tr>
           </thead>
+          <tbody>
+            {products.map((product) => (
+              <Product
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                image={product.image}
+              />
+            ))}
+          </tbody>
         </table>
       </div>
     </section>
