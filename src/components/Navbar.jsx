8b54f3305559f3831091c0f566b8fe48f7f5ae9d
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-06-24 17:18:44 -0300

    corrección de ortografía

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import "../css/Navbar.css";
+import React from 'react'
+import '../css/Navbar.css'
 function Navbar() {
   return (
     <nav className="row navbar-row-class">
       <div className="col-6 bg-black">
-        {" "}
+        {' '}
         <h2 className="p-2 ms-3 mt-2 text-white">Admin Control</h2>
       </div>
       <div className="col-6 bg-black">
-        {" "}
+        {' '}
         <dd className="py-3 text-white mt-2">
-          Pagina de administracion de recursos de Martei.
+          Página de administración de recursos de Martei.
         </dd>
       </div>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
