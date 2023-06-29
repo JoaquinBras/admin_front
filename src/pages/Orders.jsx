2325a92bedf89fc31e9dbff4c94a708b36f9ffeb
AlexTGarcia020 <alextgarcia020@gmail.com>
2023-06-29 18:41:18 -0300

    Detalles

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import SideBar from '../components/Sidebar'
 import Navbar from '../components/NavBar'
+import "../css/ordenes.css"
 
 function Orders() {
   const [orders, setOrders] = useState([])
